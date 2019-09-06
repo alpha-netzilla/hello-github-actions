79ca05ea6a739781dcf204fcb81ec54a0b0d6cb1
netzilla <alpha.netzilla@gmail.com>
2019-09-06 13:53:52 +0900

    validation test

--- action-a/check_validation.sh
+++ action-a/check_validation.sh
@@ -2,7 +2,7 @@
 
 sh -c "echo Hello world my name is $MY_NAME"
 
-grep -r -E "[\"\'][0-9a-zA-Z\-]{440,}[\"\']" ./*.gs > /dev/null
+grep -r -E "[\"\'][0-9a-zA-Z\-]{40,}[\"\']" ./*.gs > /dev/null
 
 if [ $? -eq 0 ]; then
 	echo "Include invalie characters"
